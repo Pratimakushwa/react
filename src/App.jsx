fa1531e3bcc412ab449cceeea4dc8ca057af43ba
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-09-16 20:51:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Hearder from "./Header";
+
+function App(){
+// javascriptm 
+    let myname="pratima"
+    return(
+        <>
+        {/*Header/> */}
+        <Hearder></Hearder>
+         <h1>hello world</h1>
+         <p>my name is {myname}</p>
+        </>
+    )
+}
+export default App
+
+
+
+// funtion return jsx
+// react fragment <>
+// return ek hi element return krta
+//  use crly braces when we js in html
+// 
+ 
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hearder(){
+    return(
+        <>
+        <p> this is header</p>
+        </>
+    )
+}
+export default Hearder
